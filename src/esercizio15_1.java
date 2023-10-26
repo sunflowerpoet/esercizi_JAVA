aa10b69d9941d8ec599ff0e1fe8534a82981e652
sunflowerpoet <anna.simionato747@gmail.com>
2023-10-26 18:03:10 +0200

    first commit

--- src/esercizio15_1.java
+++ src/esercizio15_1.java
@@ -6,7 +6,7 @@ public class esercizio15_1 {
     public static void main (String[]args) {
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.print("Fizzbuzz");
+                System.out.println("Fizzbuzz");
             } else if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
@@ -14,5 +14,7 @@ public class esercizio15_1 {
             } else {
                 System.out.println(i);
             }
+        }
+    }
 }
 
