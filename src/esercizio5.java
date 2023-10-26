6892e5cb654a0a97559b7744712458028352ac47
sunflowerpoet <anna.simionato747@gmail.com>
2023-10-26 17:04:35 +0200

    first commit

---
+++ src/esercizio5.java
@@ -0,0 +1,16 @@
+public class esercizio5 {
+    public static void main(String args[]) {
+        int x = 15;
+        int y = 25;
+
+        System.out.println("Sum of x + y = " + somma(x , y));
+    }
+
+    public static int somma(int x, int y){
+        int z =  x +  y;
+        return z;
+
+    }
+
+}
+}
