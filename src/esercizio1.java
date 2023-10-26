8f6075c91501ac62f171a54dc950230a784b4526
sunflowerpoet <anna.simionato747@gmail.com>
2023-10-26 15:32:28 +0200

    first commit

--- src/esercizio1.java
+++ src/esercizio1.java
@@ -1,5 +1,9 @@
 public class esercizio1 {
     public static void main(String[] args) {
+        float base = 13f;
+        int height = 15;
+        float area = (base*height)/2;
 
+        System.out.println("Triangle area = " + area);
     }
 }
