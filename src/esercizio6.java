4af115ac64a9c8b8ffa41bf55589ee32f5eb3dc5
sunflowerpoet <anna.simionato747@gmail.com>
2023-10-26 17:05:23 +0200

    first commit

---
+++ src/esercizio6.java
@@ -0,0 +1,14 @@
+public class esercizio6 {
+    public static void main(String[] args) {
+
+        String myString = "My name is Anna";
+
+        int length = lengthCalc(myString);
+
+        System.out.println("The length of my string: " + length);
+    }
+    public static int lengthCalc(String input) {
+        return input.length();
+    }
+}
+}
