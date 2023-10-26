fa132f7921278f0a931aa335bdd9d9a4e2a80043
sunflowerpoet <anna.simionato747@gmail.com>
2023-10-26 18:05:25 +0200

    first commit

--- src/esercizio15.java
+++ src/esercizio15.java
@@ -2,7 +2,7 @@
 //"Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".
 //Nel caso in cui un numero sia multiplo di entrambi stampi "FizzBuzz".
 
-public class esercizio15_1 {
+public class esercizio15 {
     public static void main (String[]args) {
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
