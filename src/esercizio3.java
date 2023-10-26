dcb678a343471325a1c375de0e7054736ac8b53d
sunflowerpoet <anna.simionato747@gmail.com>
2023-10-26 17:02:34 +0200

    first commit

---
+++ src/esercizio3.java
@@ -0,0 +1,9 @@
+public class esercizio3 {
+    public static void main(String args[]) {
+        double a = 3.0;
+        double b = 2.0;
+        int somma = (int)a + (int)b;
+
+        System.out.println("The sum is " + somma);
+    }
+}
