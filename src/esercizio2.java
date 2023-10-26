bb62fdc1d202768ad2ea1420720c1c4a78df509b
sunflowerpoet <anna.simionato747@gmail.com>
2023-10-26 17:01:41 +0200

    first commit

---
+++ src/esercizio2.java
@@ -0,0 +1,10 @@
+public class esercizio2 {
+        public static void main(String args[]) {
+            char a = 'a';
+            char b = 'b';
+            String character = "" + a + b;
+
+            System.out.println("The sum of charchters is " + character);
+        }
+    }
+}
