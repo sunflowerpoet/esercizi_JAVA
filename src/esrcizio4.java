745bb1c2bc271c03bf6b34f85fceec78b4405b39
sunflowerpoet <anna.simionato747@gmail.com>
2023-10-26 17:03:24 +0200

    first commit

---
+++ src/esrcizio4.java
@@ -0,0 +1,14 @@
+public class esrcizio4 {
+    public static void main(String args[]) {
+        char char1 = 'A';
+        char char2 = 'B';
+
+        int Ascii1 = (int) char1;
+        int Ascii2 = (int) char2;
+
+        int sumAscii = Ascii1 + Ascii2;
+
+        System.out.println("The sum of the ASCII values of " + char1 + " and " + char2 + " è: " + sumAscii);
+    }
+}
+
