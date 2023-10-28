46fd7a69186b75ff364e9e9929aa382d40cb9830
sunflowerpoet <anna.simionato747@gmail.com>
2023-10-28 19:41:19 +0200

    first commit

--- src/esercizio27.java
+++ src/esercizio27.java
@@ -2,4 +2,20 @@
 una matrice riempita con dei valori a vostro piacimento
 e restituisca la somma degli elementi sulla prima riga*/
 public class esercizio27 {
+    public static void main (String[] args){
+        int [] matrix = {10, 7, 5, 3};
+        System.out.println("The sum of the elements of the matrix is: " + sumNumArr(matrix));
+        System.out.print("The elements of the matrix are: " + matrix [0]);
+        System.out.print(" " + matrix [1] + " ");
+        System.out.print(matrix [2] + " ");
+        System.out.print(matrix [3]);
+
+    }
+    public static int sumNumArr(int[] matrix) {
+        int sumArr = 0;
+        for (int element : matrix) {
+            sumArr += element;
+        }
+        return sumArr;
+    }
 }
--- src/esercizio28.java
+++ src/esercizio28.java
@@ -11,4 +11,36 @@ Matrice risultato
  3 6
  */
 public class esercizio28 {
+    public static void main (String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+        System.out.println("Starting matrix:");
+        printMatrix(matrix);
+        int[][] invertedMatrix = invertLinesColumns(matrix);
+
+        System.out.println("\nResult matrix:");
+        printMatrix(invertedMatrix);
+    }
+    public static int[][] invertLinesColumns(int[][] matrix) {
+        int lines = matrix.length;
+        int columns = matrix[0].length;
+        int[][] invertedMatrix = new int[columns][lines];
+
+        for (int i = 0; i < lines; i++) {
+            for (int j = 0; j < columns; j++) {
+                invertedMatrix[j][i] = matrix[i][j];
+            }
+        }
+        return invertedMatrix;
+    }
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
