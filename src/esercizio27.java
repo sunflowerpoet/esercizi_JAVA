a459707d5b326b15e10c727b517af51c9e201945
sunflowerpoet <anna.simionato747@gmail.com>
2023-10-30 10:55:12 +0100

    first commit

--- src/esercizio27.java
+++ src/esercizio27.java
@@ -1,21 +1,29 @@
+import java.util.Arrays;
+
 /*Scrivere un programma che contenga un metodo che permette di inizializzare
 una matrice riempita con dei valori a vostro piacimento
 e restituisca la somma degli elementi sulla prima riga*/
 public class esercizio27 {
-    public static void main (String[] args){
-        int [] matrix = {10, 7, 5, 3};
-        System.out.println("The sum of the elements of the matrix is: " + sumNumArr(matrix));
-        System.out.print("The elements of the matrix are: [ " + matrix [0]);
-        System.out.print(", " + matrix [1] + ", ");
-        System.out.print(matrix [2] + ", ");
-        System.out.print(matrix [3] + " ]");
-
+    public static void main (String[] args) {
+        System.out.println("Input matrix:");
+        System.out.println("The sum of the elements of the first line of the matrix is: " + sumMatrix(createMatrix()));
     }
-    public static int sumNumArr(int[] matrix) {
-        int sumArr = 0;
-        for (int element : matrix) {
-            sumArr += element;
+
+    public static int [][] createMatrix() {
+        int[][] matrix = new int[3][3];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = 3;
+            }
+            System.out.println(Arrays.toString(matrix[i]));
         }
-        return sumArr;
+        return matrix;
+    }
+    public static int sumMatrix(int[][] matrix) {
+        int sum = 0;
+        for (int i = 0; i < matrix[0].length; i++) {
+            sum = sum + matrix[0][i];
+        }
+        return sum;
     }
 }
